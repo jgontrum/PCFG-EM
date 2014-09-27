ffa06a9f18325a8b58b0e290745eea94665e8d8a
Johannes Gontrum <gontrum@uni-potsdam.de>
2014-09-27 03:42:16 +0200

    Added commandline interface. Not tested yet because boost and linkers start to annoy me.

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 #include <fstream>
 
 #include <boost/unordered_set.hpp>
+#include <boost/program_options.hpp>
+
 #include "../include/Signature.hpp"
 #include "../include/ProbabilisticContextFreeGrammar.hpp"
 #include "../include/PCFGRule.hpp"
@@ -26,61 +28,76 @@ _INITIALIZE_EASYLOGGINGPP
 int main(int argc, const char * argv[])
 {
     _START_EASYLOGGINGPP(argc, argv);
-    
     typedef boost::char_separator<char> CharSeparator;
     typedef boost::tokenizer<CharSeparator> Tokenizer;
     typedef std::vector<std::string> StringVector;
+    
+    
+    // Define and parse the program options (-> http://www.radmangames.com/programming/how-to-use-boost-program_options)
+    namespace po = boost::program_options;
+    po::options_description desc;
+    desc.add_options()
+            ("help", "Print help messages")
+            ("grammar,g", po::value<std::string>(), "Path to a PCFG.")
+            ("train,t", po::value<std::string>(), "Path to the training set with sentences seperated by newlines.")
+            ("save,s", po::value<std::string>(), "Path to save the altered grammar")
+            ("out,o", "Output the grammar after the training.")
+            ("iterations,i", po::value<unsigned>(), "Amount of training circles to perform. (Default: 3)")
+            ;
 
-    std::ifstream grammar_file;
+    po::variables_map vm; 
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm); 
 
-    grammar_file.open("examples/grammar_no_prob.pcfg", std::ifstream::in);
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
 
-    if (grammar_file) {
-        ProbabilisticContextFreeGrammar grammar(grammar_file);
-        
-        std::ifstream training_file;
-        training_file.open("examples/test_training.txt", std::ifstream::in);
-        
-        if (training_file) {
-            
-            EMTrainer trainer(grammar, training_file);
-            
-            std::cout << grammar << "\n\n";
-            
-            trainer.train(1);
+    if (vm.count("grammar")) {
+        if (vm.count("train")) { 
+            std::ifstream grammar_file;
+            grammar_file.open(vm["grammar"].as<std::string>(), std::ifstream::in);
+            if (grammar_file) {
 
-            std::cout << grammar << "\n\n";
+                std::ifstream training_file;
+                training_file.open(vm["train"].as<std::string>(), std::ifstream::in);
 
-            trainer.train(1);
+                if (training_file) {
+                    // Read in grammar
+                    ProbabilisticContextFreeGrammar grammar(grammar_file);
 
-            std::cout << grammar << "\n\n";
-//            
-//            std::string sentence = "Maria mag Hans";
-//            Tokenizer tokens(sentence, CharSeparator("\t "));
-//            StringVector vtokens(tokens.begin(), tokens.end());
-//            InsideOutsideCache io_cache(grammar);
-//
-//
-//            std::string sentence2 = "mag";
-//            Tokenizer tokens2(sentence, CharSeparator("\t "));
-//            StringVector vtokens2(tokens.begin(), tokens.end());
-//
-//
-//            InsideOutsideCalculator em(io_cache, vtokens2);
-//
-//            std::cout << em.calculate_outside(grammar.get_signature().resolve_symbol("V"), 1, 1);
-//            //        std::cout << em.calculate_inside(grammar.get_start_symbol(), 0, vtokens.size() - 1);
-            
+                    // Initialize the EMTrainer
+                    EMTrainer trainer(grammar, training_file);
+                    
+                    // Perform the actual training
+                    if (vm.count("iterations")) {
+                        trainer.train(vm["iterations"].as<unsigned>());
+                    } else {
+                        trainer.train(3); // default
+                    }
+
+                    // If wanted, print the new grammar to cout
+                    if (vm.count("out")) { 
+                        std::cout << grammar;
+                    }
+                    // If wanted, save the new grammar in a file
+                    if (vm.count("save")) {
+                        // ...
+                    }
+                } else {
+                    std::cerr << "Could not read training data.";
+                }
+            } else {
+                std::cerr << "Could not read PCFG.";
+            }
         } else {
-            std::cerr << "error reading file training";
+            std::cerr << "Please specify a training file.\n";
+            return 1;
         }
     } else {
-            std::cerr << "error reading file ";
+        std::cerr << "Please specify a grammar.\n";
     }
-    // Signature<std::string> sig;
-    // PCFGRule r1("S --> NP VP [0.3]", sig);
-    // std::cerr << r1 << "\n";
-    // std::cerr << sig;
-
+    
 }
 
